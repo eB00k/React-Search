56cb192cc032a94f59629e2fa3f5553ee79372ad
eB00k <dasakatov8@gmai.com>
2023-03-09 17:34:05 +0600

    Basic Search Function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./app.scss";
+import { usersData } from "./users";
 
 function App() {
+  const [query, setQuery] = useState("");
+  console.log(query);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="text"
+        placeholder="Search"
+        className="search"
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <ul className="list">
+        {usersData
+          .filter((user) => user.name.toLowerCase().includes(query.toLowerCase()))
+          .map((user) => (
+            <li key={user.id} className="listItem">
+              {user.name}
+            </li>
+          ))}
+      </ul>
     </div>
   );
 }
